ac6a2fe1d9bb2dce14c51c1f387508e9b94ce76a
Marco Miller <marco.miller@ericsson.com>
2021-08-18 17:47:49 -0400

    Add initial pytests starting with TSP trace client
    Add tracecompass-test-traces as a submodule, to provision these pytests
    with compass-standard kernel traces (test data, similar to resources).
    
    Add pytest-related setup and execution instructions to README if needed.
    Update .gitignore with ./__pycache__ generated by running pytest herein.
    
    A manually spawned trace server, as described in README, is required to
    successfully run these tests. This can eventually be replaced with a
    mocked server, making these tests more unit than integration currently.
    Mocking being likely more expensive, this current local server prevails
    for now.
    
    This change therefore semi-automates the testing of TSP traces endpoints
    currently implemented by this python client library. More endpoints are
    to be covered through follow-up commits. The existing ./tsp-cli-client
    script remains as the fully manual counterpart, compared to these tests.
    
    Signed-off-by: Marco Miller <marco.miller@ericsson.com>

---
+++ test_tsp.py
@@ -0,0 +1,89 @@
+# The MIT License (MIT)
+#
+# Copyright (C) 2021 - Ericsson
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from tsp.tsp_client import TspClient
+import os
+import pytest
+
+
+class TestTspClient:
+    # Not a pytest fixture so that VS Code may find its definitions.
+    tsp_client = TspClient('http://localhost:8080/tsp/api/')
+
+    @pytest.fixture(scope='module')
+    def kernel(self):
+        return f'{os.getcwd()}/tracecompass-test-traces/ctf/src/main/resources/kernel'
+
+    @pytest.fixture(scope='module')
+    def other(self):
+        return f'{os.getcwd()}/tracecompass-test-traces/ctf/src/main/resources/kernel_vm'
+
+    def test_fetch_traces_none(self):
+        response = self.tsp_client.fetch_traces()
+        assert response.status_code == 200
+        assert not response.model.traces
+
+    def test_open_trace_twice_then_delete(self, kernel, other):
+        response = self.tsp_client.open_trace(os.path.basename(kernel), kernel)
+        assert response.status_code == 200
+        response = self.tsp_client.open_trace(os.path.basename(other), other)
+        assert response.status_code == 200
+
+        response = self.tsp_client.fetch_traces()
+        assert len(response.model.traces) == 2
+        # Delete each previously opened trace to clean server for next run.
+        for trace in response.model.traces:
+            # Do not also delete the trace from disk; file part of this repo.
+            response = self.tsp_client.delete_trace(trace.UUID, False)
+            assert response.status_code == 200
+
+    def test_fetch_opened_trace_then_delete(self, kernel):
+        response = self.tsp_client.open_trace(os.path.basename(kernel), kernel)
+        assert response.status_code == 200
+        expected_uuid = response.model.UUID
+
+        response = self.tsp_client.fetch_trace(expected_uuid)
+        assert response.status_code == 200
+        assert response.model.UUID == expected_uuid
+
+        response = self.tsp_client.delete_trace(expected_uuid, False)
+        assert response.status_code == 200
+
+    def test_opened_trace_deleted(self, kernel):
+        response = self.tsp_client.open_trace(os.path.basename(kernel), kernel)
+        assert response.status_code == 200
+        trace_uuid = response.model.UUID
+
+        response = self.tsp_client.delete_trace(trace_uuid, False)
+        assert response.status_code == 200
+
+        response = self.tsp_client.fetch_trace(trace_uuid)
+        assert response.status_code == 404
+
+    def test_opened_trace_deleted_with_cache(self, kernel):
+        response = self.tsp_client.open_trace(os.path.basename(kernel), kernel)
+        assert response.status_code == 200
+        trace_uuid = response.model.UUID
+
+        # Also remove cache for this trace, but still not its file on disk.
+        response = self.tsp_client.delete_trace(trace_uuid, False, True)
+        assert response.status_code == 200
